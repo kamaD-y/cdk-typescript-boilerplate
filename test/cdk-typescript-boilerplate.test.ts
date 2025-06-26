837635ab56dfebb59769732f8b307b861ef1c6d3
kamaD-y <yk28japaaaan@gmail.com>
2025-06-27 08:26:35 +0900

    Initial commit

---
+++ test/cdk-typescript-boilerplate.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTypescriptBoilerplate from '../lib/cdk-typescript-boilerplate-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-typescript-boilerplate-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTypescriptBoilerplate.CdkTypescriptBoilerplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
